6b0b64569ac34294b55a752ef72b22ec81d427c0
RuslanKravchuk1990 <meladzeruslan164@gmail.com>
2022-12-27 19:32:04 +0200

    New commit

--- exemple.py
+++ exemple.py
@@ -2,15 +2,29 @@ class Point:
     color = 'red'
     circle = 2
 
-    class Car:
-        def __init__(self, color, mileage):
-            self.color = color
-            self.mileage = mileage
+    def __init__(self, a, b):
+        self.x = a
+        self.y = b
 
-    blue_car = Car(color='blue', mileage=20_000)
-    red_car = Car(color='red', mileage=30_000)
-    for car in (blue_car, red_car):
-        print(f"The {car.color} car has {car.mileage:,} miles")
+    def set_coords(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_coords(self):
+        return self.x, self.y
+
+pt = Point(1, 2)
+print(pt.__dict__)
+
+class Car:
+    def __init__(self, color, mileage):
+        self.color = color
+        self.mileage = mileage
+
+blue_car = Car(color='blue', mileage=20_000)
+red_car = Car(color='red', mileage=30_000)
+for car in (blue_car, red_car):
+    print(f"The {car.color} car has {car.mileage:,} miles")
 
     # position, name, age, level, salary
     se1 = ["Sofware engineer", "Kiril", 23, "junior", 5000]
