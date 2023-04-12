48a2f32f1a305583a117890dcbb838ca27456222
RuslanKravchuk1990 <105359066+RuslanKravchuk1990@users.noreply.github.com>
2023-04-12 15:54:34 +0300

    Add files via upload

--- exemple.py
+++ exemple.py
@@ -17,6 +17,33 @@ class Point:
 pt = Point(1, 2 )
 print(pt.__dict__)
 
+class Color:
+    red = 0
+    green = 0
+    blue = 0
+
+    def __init__(self, r, g, b):
+        red = r
+        green = g
+        blue = b
+
+    def toHex(self):
+        return '#%02x%02x%02x' % (red, green, blue)
+
+class ColorAlpha(Color):
+    alpha = 1
+
+    def __init__(self, r, g, b, a):
+        red = r
+        green = g
+        blue = b
+        alpha = a
+
+gray = Color(80, 80, 80)
+
+red = ColorAlpha(255, 0, 0, .5)
+
+
 class Car:
     def __init__(self, color, mileage):
         self.color = color
